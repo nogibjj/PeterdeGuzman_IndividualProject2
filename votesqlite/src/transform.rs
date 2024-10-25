2e4e626f7467c393a680323e267b130a16f12b90
PeterdeGuzman <pdeguz01@Peters-MacBook-Pro.local>
2024-10-25 10:36:05 -0400

    updating load

--- votesqlite/src/transform.rs
+++ votesqlite/src/transform.rs
@@ -1,1 +1,20 @@
-// Creates Transform functions
+// Creates Transform and Load functions
+
+
+
+
+
+//Load Polling Places data
+pub fn load_data_from_csv()
+
+
+
+
+
+
+
+
+//Future expansion:
+//Transform Windows-1521 encoded files to UTF-16
+//Load voter registration dataset
+
