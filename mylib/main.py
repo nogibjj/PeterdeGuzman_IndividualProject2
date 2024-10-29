070e273e9b4d7afdf5df4e3ef45ed54d24ba064c
PeterdeGuzman <pdeguz01@peters-mbp-2.lan>
2024-10-29 00:05:58 -0400

    updating py

--- mylib/main.py
+++ mylib/main.py
@@ -4,5 +4,6 @@ from lib import load_pollingplaces
 if __name__ == "__main__":
 
     load_pollingplaces(
-        dataset=f"{main_directory}/data/pollingplaces_2020.csv", year=2020
+        dataset="/Users/pdeguz01/Documents/git/PeterdeGuzman_IndividualProject2/votesqlite/data/polling_place_20201103.csv",
+        year=2020,
     )
